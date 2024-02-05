4e492b303806ac024ed271ded9be0c6a3686eaa2
Josh <josh_sverdlov1@baylor.edu>
2024-02-05 15:40:43 -0600

    Dictionary Practice

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,23 +10,35 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(len(phonebook))
 
+print(phonebook['Chris'])
 
+my_dictionary = {} #this will create an empty dictionary
 
+my_dictionary2 = dict(m=8, n=9) # m and n are keys and 8 and 9 are their corresponding values
+
+print(my_dictionary2)
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
 
+if name in phonebook:
+    print(f"Name: {name} Phone Number: {[name]}")
+else:
+    print(f"{name} is not in the phone book")
 
 
 
@@ -46,9 +58,12 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook['Joe'] = '555-0123'   #this will add a new ket value pair
+phonebook['Chris'] = '555-4444'
 
-
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
@@ -58,12 +73,15 @@ print()
 
 
 
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+print(phonebook)
 
+del phonebook['Chris']
+
+print(phonebook)
 
 
 print()
@@ -79,8 +97,11 @@ print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for x in phonebook:
+    print(f"The key is {x} and the value is {phonebook[x]}") #iterates through the keys
 
-
+for value in phonebook.values(): #iterates through the value
+    print(value)
 
 
 
@@ -89,7 +110,7 @@ print('*****  end section 5 ********')
 print()
 
 
-
+'''
 
 
 print()
